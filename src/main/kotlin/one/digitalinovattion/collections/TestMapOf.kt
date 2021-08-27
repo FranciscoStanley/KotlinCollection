419469fb503163784f12d31d8a000bbd7a2850b2
Francisco Stanley Rodrigues Albuquerque <38919705+FranciscoStanley@users.noreply.github.com>
2021-08-27 11:09:53 -0300

    Create Test map class.

--- src/main/kotlin/one/digitalinovattion/collections/TestMapOf.kt
+++ src/main/kotlin/one/digitalinovattion/collections/TestMapOf.kt
@@ -1,2 +1,26 @@
 package one.digitalinovattion.collections
 
+fun main(){
+
+    //Declarando as variavéis
+    val pair: Pair<String, Double> = Pair("Albuquerque", 3600.0)
+    val map1 = mapOf(pair)
+
+    //Processamento e saída de dados usando forEach para iterar.
+    map1.forEach { (k, v) -> println("Chave: $k - valor: $v") }
+
+    println("----------------------------------")
+
+    //Declarando as variavéis para o map2.
+    val map2 = mapOf(
+        "Albuquerque" to 3600.0,
+        "Sabrina" to 4600.0
+    )
+
+    //Processamento e saída de dados usando forEach para iterar.
+    map2.forEach { (k, v) -> println("Chave: $k - valor: $v") }
+
+
+}
+
+//O map é uma coleção de chaves e seus valores.
